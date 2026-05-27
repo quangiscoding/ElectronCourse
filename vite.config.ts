ff6f888284c649a4fefe00564586ac6eb31c3128
quangiscoding <quangnguyen2005gt08@gmail.com>
2026-05-27 10:27:07 +0700

    Add vite build config for dist-react; add dist-react in .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // When "npm run build" runs (usually mapped to "vite build"
+  // inside package.json), Vite reads this config file.
+  //
+  // The "outDir" option tells Vite where to place
+  // the final production build files.
+  //
+  // So instead of generating:
+  //   dist/
+  //
+  // Vite will generate:
+  //   dist-react/
+  build: {
+    outDir: "dist-react",
+  },
+});
