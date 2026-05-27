90291fe1a8933c9bd8a039808929df167482c38d
quangiscoding <quangnguyen2005gt08@gmail.com>
2026-05-27 13:52:40 +0700

    Add entry file main.js; Add base url and build outDir for vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: "./",
   // When "npm run build" runs (usually mapped to "vite build"
   // inside package.json), Vite reads this config file.
   //
