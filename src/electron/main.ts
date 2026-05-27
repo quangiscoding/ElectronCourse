856bd99a52f375cd5158d498207a7e8da0c607a4
quangiscoding <quangnguyen2005gt08@gmail.com>
2026-05-27 14:32:41 +0700

    Add ts config for electron

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,6 +1,8 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 
+type test = string;
+
 app.on("ready", () => {
   const window = new BrowserWindow({});
   window.loadFile(path.join(app.getAppPath(), "dist-react", "index.html"));
